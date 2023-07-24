759b0ab7f8e22e23b9fadb23b173119662bbf50c
Q Xu <m3196253580@gmail.com>
2023-07-24 16:18:25 +0800

    set tab equals to 4 space

--- init.lua
+++ init.lua
@@ -8,3 +8,11 @@
 
 -- set a column line to indicate line changing of code
 vim.opt.colorcolumn = "80"
+
+-- set tab = 4 spaces
+-- show existing tab with 4 spaces width
+vim.opt.tabstop = 4
+-- when indenting with '>', use 4 spaces width
+vim.opt.shiftwidth = 4
+-- On pressing tab, insert 4 spaces
+vim.opt.expandtab = true
