99fef0f0621e1955d976f6d4e4f92e4e9db4cbd7
Q Xu <m3196253580@gmail.com>
2023-11-19 22:01:05 +0800

    fix ruff config in windows

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,22 +4,17 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
--- local servers = { "clangd" }
+local servers = { "clangd" }
 
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = on_attach,
---     capabilities = capabilities,
---   }
--- end
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
 
 -- 
 -- lspconfig.pyright.setup { blabla}
-
-lspconfig.clangd.setup {
-on_attach = on_attach,
-capabilities = capabilities,
-}
 
 -- pylsp settings
 lspconfig.pylsp.setup {
--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -11,7 +11,7 @@ local ruff_config
 if vim.loop.os_uname().sysname == "Linux" then
     ruff_config = {"--config=/home/xu/.config/nvim/lua/custom/ruff/pyproject.toml"}
 else
-    ruff_config = {"--config=$HOME\\AppData\\Local\\nvim\\lua\\custom\\ruff\\pyproject.toml"}
+    ruff_config = {"--config=".. os.getenv("UserProfile") .. "\\AppData\\Local\\nvim\\lua\\custom\\ruff\\pyproject.toml"}
 end
 
 local sources = {
